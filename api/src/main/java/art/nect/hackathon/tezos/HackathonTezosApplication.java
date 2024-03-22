076ac9d2c5e2c6178b88b87bafb57073d2440830
Caceresenzo <caceresenzo1502@gmail.com>
2024-03-22 21:37:39 +0100

    feat(api): add

---
+++ api/src/main/java/art/nect/hackathon/tezos/HackathonTezosApplication.java
@@ -0,0 +1,13 @@
+package art.nect.hackathon.tezos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonTezosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonTezosApplication.class, args);
+	}
+
+}
