9aeb5462bca0484b9afa7768a6941ff2254a2ad4
Caceresenzo <caceresenzo1502@gmail.com>
2024-03-23 15:38:44 +0100

    feat(nft): add contract

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,6 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+import dotenv from "dotenv";
 
+dotenv.config()
 if (!process.env.PRIVATE_KEY) {
   throw new Error("PRIVATE_KEY is not set");
 }
@@ -14,6 +17,21 @@ const config: HardhatUserConfig = {
       accounts: [process.env.PRIVATE_KEY],
     },
   },
+  etherscan: {
+    enabled: true,
+    apiKey: "x",
+    customChains: [
+      {
+        network: "etherlink-testnet",
+        chainId: 128123,
+        urls: {
+          browserURL: "https://testnet-explorer.etherlink.com",
+          apiURL: "https://testnet-explorer.etherlink.com/api",
+        }
+      }
+    ],
+  },
+  
 };
 
 export default config;
