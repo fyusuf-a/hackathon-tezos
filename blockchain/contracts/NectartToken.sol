2ad097db4266ae93a7671019c095e116bf51cf4d
Caceresenzo <caceresenzo1502@gmail.com>
2024-03-24 02:10:50 +0100

    feat(nft): add `ERC721Enumerable` extension

--- blockchain/contracts/NectartToken.sol
+++ blockchain/contracts/NectartToken.sol
@@ -3,19 +3,38 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NectartToken is ERC721, Ownable {
+contract NectartToken is ERC721, ERC721Enumerable, Ownable {
     uint256 private _nextTokenId;
 
-    constructor()
-        ERC721("Nectart", "NECTART")
-        Ownable(msg.sender)
-    {}
+    constructor() ERC721("Nectart", "NECTART") Ownable(msg.sender) {}
 
     /* allow anyone to mint for testing purposes */
     function safeMint(address to) public {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
+    }
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
